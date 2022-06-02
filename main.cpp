983b2a87e92050c9210b6e3881db6969eaef8fe6
James <jjcastle@utexas.edu>
2022-06-01 21:57:20 -0500

    Added super rough draft idea for reverseArray for concept testing.

--- main.cpp
+++ main.cpp
@@ -19,10 +19,12 @@ vector<int> reverseArray(vector<int> a) {
     //create a count controlled loop that starts at the first index i and sets the element to the value of  n-i.
     // e.g. index 0 should be replaced with the value at index n-0. For array of size 5, index 4 should be replaced with
     // the value at  n-4.
-
+    for (int i = 0; i < a.size(); i++) {
+        a[i] = (a.size()-1);
+    }
 
     //return the array
-
+    return a;
 }
 
 int main()
